51adc987a3d2906c41d528de88db332b3f1f78c0
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2025-10-31 11:21:15 -0700

    u

--- src/lib/tools/search.ts
+++ src/lib/tools/search.ts
@@ -12,6 +12,12 @@ const searchTool = tool({
         query: z.string().describe('The query to search for'),
     }),
     execute: async ({ query }: { query: string }) => {
+            if (!import.meta.env?.EXASEARCH_API_KEY) {
+                const msg = 'Error API KEY EXASEARCH_API_KEY not found in ENV'
+                console.error(msg)
+                return { status: 'error', message: msg}
+            }
+
             const searchResult = await exa.getContents(
                 [query], {
                     apiKey: process.env.EXASEARCH_API_KEY || import.meta.env.EXA_SEARCH_API_KEY || '',
