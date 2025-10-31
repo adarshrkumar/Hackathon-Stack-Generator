809140f21492d6131be7fdc9bec3e0553551a86c
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2025-10-31 11:21:03 -0700

    u

--- src/lib/tools/getPageContent.ts
+++ src/lib/tools/getPageContent.ts
@@ -40,6 +40,12 @@ const getPageContentTool = tool({
                 };
             }
 
+            if (!import.meta.env?.EXASEARCH_API_KEY) {
+                const msg = 'Error API KEY EXASEARCH_API_KEY not found in ENV'
+                console.error(msg)
+                return { status: 'error', message: msg}
+            }
+
             const searchResult = await exa.getContents(urls, {
                 apiKey: process.env.EXA_SEARCH_API_KEY || import.meta.env.EXA_SEARCH_API_KEY || '',
                 text: includeText ? true : undefined,
